2752279f969186c28cdce0e0d8a6bf1e8dcafff1
ohadavitan <ohadiii100@gmail.com>
2024-11-13 17:15:20 +0200

    dasd

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <Header />
+      <Home />
+      <Footer />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+// import Header from './components/Header.vue';
+
+//
+</script>
+
+<style lang="scss">
+.v-main {
+  font-family: "Fredoka", sans-serif;
+  text-align: right;
+  // font-family: "Afacad", sans-serif;
+  // font-family: "Secular One", sans-serif;
+}
+</style>
